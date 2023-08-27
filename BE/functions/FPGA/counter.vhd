f841b2cf7a26f36f1c7aa32ac90f815c2ab7e871
Touftoufe <sofiane.aouci@gmail.com>
2023-08-27 14:43:58 +0200

    Update counter.vhd

--- BE/functions/FPGA/counter.vhd
+++ BE/functions/FPGA/counter.vhd
@@ -16,17 +16,10 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 
 -- --------------------- COUNTER ------------------------ --
 entity COUNTER is
